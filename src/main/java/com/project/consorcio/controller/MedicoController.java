ac64bf4b7bb69b2f3015037ba032d2b2ff9e45cc
Alex <abellezap@gmail.com>
2021-11-17 22:04:48 -0500

    cambios 18-11-2021-Alex

--- src/main/java/com/project/consorcio/controller/MedicoController.java
+++ src/main/java/com/project/consorcio/controller/MedicoController.java
@@ -6,5 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/medico")
 public class MedicoController {
-
+	
+	public void incio(){
+		
+	}
+	
+	public String datos() {
+		return "hola";
+	}
+	
 }
