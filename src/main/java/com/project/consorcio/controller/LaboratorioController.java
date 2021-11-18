ab592714d7317f9e9d38c34a4314faf2d9bfce65
Alex <abellezap@gmail.com>
2021-11-17 21:49:42 -0500

    cambios 17-11-2021-Alex

---
+++ src/main/java/com/project/consorcio/controller/LaboratorioController.java
@@ -0,0 +1,13 @@
+package com.project.consorcio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/laboratorio")
+public class LaboratorioController {
+	
+	
+	
+	
+}
